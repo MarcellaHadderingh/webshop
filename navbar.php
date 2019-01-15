4e324dd585e51e802ae6fd6172a42e574af4389e
marcella <marcellavanduyn@gmail.com>
2019-01-15 13:23:02 +0100

    php files added
    added:product toevoegen, product update, product verwijderen, koopproduct, productbewerken.

---
+++ navbar.php
@@ -0,0 +1,34 @@
+
+<?php
+    include_once("dblib.php");
+
+
+$productid = $_GET['productid'];
+$besteller = 'floris@codegorilla.nl';
+$tebetalen = 0.00;
+
+$tebetalen = dblookup("producten", "prijs", $productid);
+
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=webshopdb", 'root', '');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $statement = $conn->prepare('INSERT INTO bestellingen (email, productid, tebetalen) VALUES (:femail, :fproductid, :ftebetalen)');
+
+    $statement->execute([
+    'femail' => $besteller,
+    'fproductid' => $productid,
+    'ftebetalen' => $tebetalen
+    ]);
+	
+}
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+	
+$conn = NULL;
+
+header("Location: index.php");
+
+
+?>
