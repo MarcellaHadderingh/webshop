ecc3780a5dea79573eef51e54e2ee8cc85ef7cc7
marcella <marcellavanduyn@gmail.com>
2019-01-15 16:24:30 +0100

    deleted and changed

--- producttoevoegen.php
+++ producttoevoegen.php
@@ -3,7 +3,7 @@
     <div class="wrapper1">
         <h1 class="productlist-title" >Product toevoegen</h1>
             <div class="updateform">
-                <form action="dbproductoevoegen.php" method="POST">
+                <form action="dbproducttoevoegen.php" method="POST">
                 <input type="text" name="productnaam" placeholder="Product naam">
                 <input type="text" name="productprijs" placeholder="Product prijs">
                 <button class="btn-update" type="submit">Voeg product toe</button>
