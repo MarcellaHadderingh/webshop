14ca6b04b4a57cc5edfa3fb4242e55165893b488
marcella <marcellavanduyn@gmail.com>
2019-01-17 15:12:19 +0100

    navabar added icon

--- navbar.php
+++ navbar.php
@@ -1,5 +1,5 @@
 <head>
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"><link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <header>
@@ -11,6 +11,7 @@
                 <li><a href="index.php">Home</a></li>
                 <li><a href="product.php">Fruit bestellen</a></li>
                 <li><a href="toonbestellingen.php">Bestellingen</a></li>
+                <li><a href="bestellen.php"><i class="fas fa-shopping-cart"></i></a></li>
             </ul>
         </nav>
     </div>
